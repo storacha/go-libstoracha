7144600f5aebc3dfeaf3b4fcde46991693eef686
Alan Shaw <alan@storacha.network>
2024-10-21 14:40:22 +0100

    feat: import assert capabilities from indexing-service

--- pkg/assert/datamodel/assert.go
+++ pkg/assert/datamodel/assert.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/ipld/go-ipld-prime"
+	"github.com/ipld/go-ipld-prime/datamodel"
 	"github.com/ipld/go-ipld-prime/schema"
 )
 
@@ -25,14 +26,80 @@ func LocationCaveatsType() schema.Type {
 	return assertTS.TypeByName("LocationCaveats")
 }
 
+func InclusionCaveatsType() schema.Type {
+	return assertTS.TypeByName("InclusionCaveats")
+}
+
+func IndexCaveatsType() schema.Type {
+	return assertTS.TypeByName("IndexCaveats")
+}
+
+func PartitionCaveatsType() schema.Type {
+	return assertTS.TypeByName("PartitionCaveats")
+}
+
+func RelationCaveatsType() schema.Type {
+	return assertTS.TypeByName("RelationCaveats")
+}
+
+func EqualsCaveatsType() schema.Type {
+	return assertTS.TypeByName("EqualsCaveats")
+}
+
+func DigestType() schema.Type {
+	return assertTS.TypeByName("Digest")
+}
+
 type RangeModel struct {
 	Offset uint64
 	Length *uint64
 }
 
 type LocationCaveatsModel struct {
-	Content  []byte
+	Content  datamodel.Node
 	Location []string
 	Range    *RangeModel
 	Space    []byte
+}
+
+type DigestModel struct {
+	Digest []byte
+}
+
+type InclusionCaveatsModel struct {
+	Content  datamodel.Node
+	Includes ipld.Link
+	Proof    *ipld.Link
+}
+
+type IndexCaveatsModel struct {
+	Content ipld.Link
+	Index   ipld.Link
+}
+
+type PartitionCaveatsModel struct {
+	Content datamodel.Node
+	Blocks  *ipld.Link
+	Parts   []ipld.Link
+}
+
+type RelationPartInclusionModel struct {
+	Content ipld.Link
+	Parts   *[]ipld.Link
+}
+
+type RelationPartModel struct {
+	Content  ipld.Link
+	Includes *RelationPartInclusionModel
+}
+
+type RelationCaveatsModel struct {
+	Content  datamodel.Node
+	Children []ipld.Link
+	Parts    []RelationPartModel
+}
+
+type EqualsCaveatsModel struct {
+	Content datamodel.Node
+	Equals  ipld.Link
 }
