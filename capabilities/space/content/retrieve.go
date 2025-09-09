bd44875c3b94f526ca6498eddf9616468b1599f8
Vicente Olmedo <vicente@storacha.network>
2025-09-09 16:43:03 +0200

    feat: add space/content/retrieve capability

---
+++ capabilities/space/content/retrieve.go
@@ -0,0 +1,129 @@
+package content
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/ipld/go-ipld-prime/datamodel"
+	"github.com/multiformats/go-multibase"
+	mh "github.com/multiformats/go-multihash"
+	"github.com/storacha/go-ucanto/core/ipld"
+	"github.com/storacha/go-ucanto/core/receipt"
+	"github.com/storacha/go-ucanto/core/result/failure"
+	"github.com/storacha/go-ucanto/core/schema"
+	"github.com/storacha/go-ucanto/ucan"
+	"github.com/storacha/go-ucanto/validator"
+
+	"github.com/storacha/go-libstoracha/capabilities/types"
+)
+
+const RetrieveAbility = "space/content/retrieve"
+
+// Range represents a range of byte offsets to retrieve.
+// `Start` is the start offset from which to extract bytes. `End` is the offset at which extraction should end.
+// Both offsets are inclusive.
+type Range struct {
+	Start uint64
+	End   uint64
+}
+
+// RetrieveCaveats represents the caveats required to perform a space/content/retrieve invocation.
+type RetrieveCaveats struct {
+	Digest mh.Multihash
+	Range  Range
+}
+
+func (ac RetrieveCaveats) ToIPLD() (datamodel.Node, error) {
+	return ipld.WrapWithRecovery(&ac, RetrieveCaveatsType(), types.Converters...)
+}
+
+var RetrieveCaveatsReader = schema.Struct[RetrieveCaveats](RetrieveCaveatsType(), nil, types.Converters...)
+
+// RetrieveOk represents the result of a successful space/content/retrieve invocation.
+type RetrieveOk struct{}
+
+func (ao RetrieveOk) ToIPLD() (datamodel.Node, error) {
+	return ipld.WrapWithRecovery(&ao, RetrieveOkType(), types.Converters...)
+}
+
+type RetrieveReceipt receipt.Receipt[RetrieveOk, failure.Failure]
+
+type RetrieveReceiptReader receipt.ReceiptReader[RetrieveOk, failure.Failure]
+
+func NewRetrieveReceiptReader() (RetrieveReceiptReader, error) {
+	return receipt.NewReceiptReader[RetrieveOk, failure.Failure](contentSchema)
+}
+
+var RetrieveOkReader = schema.Struct[RetrieveOk](RetrieveOkType(), nil, types.Converters...)
+
+// Retrieve is a capability that allows the agent to retrieve a byte range from a blob in a space.
+var Retrieve = validator.NewCapability(
+	RetrieveAbility,
+	schema.DIDString(),
+	RetrieveCaveatsReader,
+	func(claimed, delegated ucan.Capability[RetrieveCaveats]) failure.Failure {
+		fail := equalWith(claimed, delegated)
+		if fail != nil {
+			return fail
+		}
+
+		fail = equalDigest(claimed, delegated)
+		if fail != nil {
+			return fail
+		}
+
+		return validRange(claimed, delegated)
+	},
+)
+
+// equalWith validates that the claimed capability's `with` field matches the delegated one.
+func equalWith(claimed, delegated ucan.Capability[RetrieveCaveats]) failure.Failure {
+	if claimed.With() != delegated.With() {
+		return schema.NewSchemaError(fmt.Sprintf(
+			"Resource '%s' doesn't match delegated '%s'",
+			claimed.With(), delegated.With(),
+		))
+	}
+
+	return nil
+}
+
+// equalDigest validates that the claimed digest capability matches the delegated one.
+func equalDigest(claimed, delegated ucan.Capability[RetrieveCaveats]) failure.Failure {
+	claimedDigest := claimed.Nb().Digest
+	delegatedDigest := delegated.Nb().Digest
+
+	// Check if the claimed digest matches
+	if !bytes.Equal(delegatedDigest, claimedDigest) {
+		claimedDigest, _ := multibase.Encode(multibase.Base58BTC, claimedDigest)
+		delegatedDigest, _ := multibase.Encode(multibase.Base58BTC, delegatedDigest)
+
+		return schema.NewSchemaError(fmt.Sprintf(
+			"Digest %s violates imposed %s constraint",
+			claimedDigest, delegatedDigest,
+		))
+	}
+
+	return nil
+}
+
+func validRange(claimed, delegated ucan.Capability[RetrieveCaveats]) failure.Failure {
+	claimedRange := claimed.Nb().Range
+	delegatedRange := delegated.Nb().Range
+
+	if claimedRange.Start < delegatedRange.Start {
+		return schema.NewSchemaError(fmt.Sprintf(
+			"Start offset %d violates imposed %d constraint",
+			claimedRange.Start, delegatedRange.Start,
+		))
+	}
+
+	if claimedRange.End > delegatedRange.End {
+		return schema.NewSchemaError(fmt.Sprintf(
+			"End offset %d violates imposed %d constraint",
+			claimedRange.End, delegatedRange.End,
+		))
+	}
+
+	return nil
+}
---
+++ capabilities/space/content/retrieve_test.go
@@ -0,0 +1,39 @@
+package content_test
+
+import (
+	"testing"
+
+	"github.com/storacha/go-libstoracha/capabilities/space/content"
+	"github.com/storacha/go-libstoracha/testutil"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRoundTripRetrieveCaveats(t *testing.T) {
+	digest := testutil.RandomMultihash(t)
+	nb := content.RetrieveCaveats{
+		Digest: digest,
+		Range: content.Range{
+			Start: 123,
+			End:   456,
+		},
+	}
+
+	node, err := nb.ToIPLD()
+	require.NoError(t, err)
+
+	rnb, err := content.RetrieveCaveatsReader.Read(node)
+	require.NoError(t, err)
+	require.Equal(t, nb, rnb)
+}
+
+func TestRoundTripRetrieveOk(t *testing.T) {
+	ok := content.RetrieveOk{}
+
+	node, err := ok.ToIPLD()
+	require.NoError(t, err)
+
+	rok, err := content.RetrieveOkReader.Read(node)
+	require.NoError(t, err)
+	require.Equal(t, ok, rok)
+}
