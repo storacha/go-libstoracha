fe43bf3a908e19db6f034b19459efa849cc902c3
Vicente Olmedo <vicente@storacha.network>
2025-09-12 15:34:51 +0200

    feat: add space/content/retrieve capability (#46)
    A first implementation of the capability, we can work from here if the
    spec changes before merge.
    
    Note: I took the liberty of using just `digest` in the caveats, as per
    https://github.com/storacha/specs/pull/139/files#r2333601709. I'll
    change it if that's not the final form of the spec.

---
+++ capabilities/space/content/retrieve_test.go
@@ -0,0 +1,63 @@
+package content_test
+
+import (
+	"testing"
+
+	"github.com/storacha/go-libstoracha/capabilities/space/content"
+	"github.com/storacha/go-libstoracha/testutil"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRoundTripRetrieveCaveats(t *testing.T) {
+	digest := testutil.RandomMultihash(t)
+	nb := content.RetrieveCaveats{
+		Blob: content.BlobDigest{
+			Digest: digest,
+		},
+		Range: content.Range{
+			Start: 123,
+			End:   456,
+		},
+	}
+
+	node, err := nb.ToIPLD()
+	require.NoError(t, err)
+
+	rnb, err := content.RetrieveCaveatsReader.Read(node)
+	require.NoError(t, err)
+	require.Equal(t, nb, rnb)
+}
+
+func TestRoundTripRetrieveOk(t *testing.T) {
+	ok := content.RetrieveOk{}
+
+	node, err := ok.ToIPLD()
+	require.NoError(t, err)
+
+	rok, err := content.RetrieveOkReader.Read(node)
+	require.NoError(t, err)
+	require.Equal(t, ok, rok)
+}
+
+func TestRoundTripNotFoundError(t *testing.T) {
+	expectError := content.NewNotFoundError("blob not found error")
+
+	node, err := expectError.ToIPLD()
+	require.NoError(t, err)
+
+	actualError, err := content.NotFoundErrorReader.Read(node)
+	require.NoError(t, err)
+	require.Equal(t, expectError, actualError)
+}
+
+func TestRoundTripRangeNotSatisfiableError(t *testing.T) {
+	expectError := content.NewRangeNotSatisfiableError("some range not satisfiable error")
+
+	node, err := expectError.ToIPLD()
+	require.NoError(t, err)
+
+	actualError, err := content.RangeNotSatisfiableErrorReader.Read(node)
+	require.NoError(t, err)
+	require.Equal(t, expectError, actualError)
+}
---
+++ capabilities/space/content/schema.go
@@ -0,0 +1,39 @@
+package content
+
+import (
+	// for schema embed
+	_ "embed"
+	"fmt"
+
+	"github.com/ipld/go-ipld-prime/schema"
+	"github.com/storacha/go-libstoracha/capabilities/types"
+)
+
+//go:embed content.ipldsch
+var contentSchema []byte
+
+var contentTS = mustLoadTS()
+
+func mustLoadTS() *schema.TypeSystem {
+	ts, err := types.LoadSchemaBytes(contentSchema)
+	if err != nil {
+		panic(fmt.Errorf("loading content schema: %w", err))
+	}
+	return ts
+}
+
+func RetrieveCaveatsType() schema.Type {
+	return contentTS.TypeByName("RetrieveCaveats")
+}
+
+func RetrieveOkType() schema.Type {
+	return contentTS.TypeByName("RetrieveOk")
+}
+
+func NotFoundErrorType() schema.Type {
+	return contentTS.TypeByName("NotFoundError")
+}
+
+func RangeNotSatisfiableErrorType() schema.Type {
+	return contentTS.TypeByName("RangeNotSatisfiableError")
+}
