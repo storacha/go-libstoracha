bd8458b060b5f0b08008b12e35cfd26901828f18
Vicente Olmedo <vicente@storacha.network>
2025-09-15 19:39:24 +0200

    feat: add space/egress/track and space/egress/consolidate capabilities

---
+++ capabilities/space/egress/track_test.go
@@ -0,0 +1,39 @@
+package egress_test
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/storacha/go-libstoracha/capabilities/space/egress"
+	"github.com/storacha/go-libstoracha/testutil"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRoundTripTrackCaveats(t *testing.T) {
+	batchCID := testutil.RandomCID(t)
+	endpoint, _ := url.Parse("http://piri.com/receipts/{cid}")
+	nb := egress.TrackCaveats{
+		Receipts: batchCID,
+		Endpoint: endpoint,
+	}
+
+	node, err := nb.ToIPLD()
+	require.NoError(t, err)
+
+	rnb, err := egress.TrackCaveatsReader.Read(node)
+	require.NoError(t, err)
+	require.Equal(t, nb.Receipts.String(), rnb.Receipts.String())
+	require.Equal(t, nb.Endpoint.String(), rnb.Endpoint.String())
+}
+
+func TestRoundTripTrackError(t *testing.T) {
+	trackErr := egress.NewTrackError("some egress track error")
+
+	node, err := trackErr.ToIPLD()
+	require.NoError(t, err)
+
+	readTrackErr, err := egress.TrackErrorReader.Read(node)
+	require.NoError(t, err)
+	require.Equal(t, trackErr, readTrackErr)
+}
