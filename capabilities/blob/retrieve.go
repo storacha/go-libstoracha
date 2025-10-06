35b23d92f0a51c41c411ea0fef88c6c19d8ef5ee
Alan Shaw <alan@storacha.network>
2025-10-06 14:33:46 +0100

    feat: add blob/retrieve capability definition

---
+++ capabilities/blob/retrieve.go
@@ -0,0 +1,85 @@
+package blob
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/ipld/go-ipld-prime/datamodel"
+	"github.com/multiformats/go-multihash"
+	"github.com/storacha/go-libstoracha/capabilities/types"
+	"github.com/storacha/go-libstoracha/digestutil"
+	"github.com/storacha/go-ucanto/core/ipld"
+	"github.com/storacha/go-ucanto/core/receipt"
+	"github.com/storacha/go-ucanto/core/result/failure"
+	"github.com/storacha/go-ucanto/core/schema"
+	"github.com/storacha/go-ucanto/ucan"
+	"github.com/storacha/go-ucanto/validator"
+)
+
+const RetrieveAbility = "blob/retrieve"
+
+// Blob represents a blob to be retrieved.
+type Blob struct {
+	// Digest is the multihash of the blob bytes, uniquely identifying the blob.
+	Digest multihash.Multihash
+}
+
+// RetrieveCaveats are the caveats required to perform an blob/retrieve invocation.
+type RetrieveCaveats struct {
+	Blob Blob
+}
+
+func (gc RetrieveCaveats) ToIPLD() (datamodel.Node, error) {
+	return ipld.WrapWithRecovery(&gc, RetrieveCaveatsType(), types.Converters...)
+}
+
+var RetrieveCaveatsReader = schema.Struct[RetrieveCaveats](RetrieveCaveatsType(), nil, types.Converters...)
+
+// RetrieveOk represents the successful response for a blob/retrieve invocation.
+type RetrieveOk struct{}
+
+func (gok RetrieveOk) ToIPLD() (datamodel.Node, error) {
+	return ipld.WrapWithRecovery(&gok, RetrieveOkType())
+}
+
+var RetrieveOkReader = schema.Struct[RetrieveOk](RetrieveOkType(), nil)
+
+type RetrieveError struct {
+	Name    string
+	Message string
+}
+
+func (ge RetrieveError) ToIPLD() (datamodel.Node, error) {
+	return ipld.WrapWithRecovery(&ge, RetrieveErrorType())
+}
+
+type RetrieveReceipt receipt.Receipt[RetrieveOk, RetrieveError]
+type RetrieveReceiptReader receipt.ReceiptReader[RetrieveOk, RetrieveError]
+
+func NewRetrieveReceiptReader() (RetrieveReceiptReader, error) {
+	return receipt.NewReceiptReaderFromTypes[RetrieveOk, RetrieveError](RetrieveOkType(), RetrieveErrorType())
+}
+
+// Retrieve is a service capability that allows an authorized agent to retrieve
+// blob bytes from another agent that is hosting/storing the blob. i.e. there is
+// no space authorization - the resource is the agent storing the blob.
+var Retrieve = validator.NewCapability(
+	RetrieveAbility,
+	schema.DIDString(),
+	RetrieveCaveatsReader,
+	RetrieveDerive,
+)
+
+func RetrieveDerive(claimed, delegated ucan.Capability[RetrieveCaveats]) failure.Failure {
+	if fail := validator.DefaultDerives(claimed, delegated); fail != nil {
+		return fail
+	}
+	if !bytes.Equal(delegated.Nb().Blob.Digest, claimed.Nb().Blob.Digest) {
+		return schema.NewSchemaError(fmt.Sprintf(
+			"claimed digest %v doesn't match delegated digest %v",
+			digestutil.Format(claimed.Nb().Blob.Digest),
+			digestutil.Format(delegated.Nb().Blob.Digest),
+		))
+	}
+	return nil
+}
---
+++ capabilities/blob/retrieve_test.go
@@ -0,0 +1,117 @@
+package blob_test
+
+import (
+	"testing"
+
+	"github.com/storacha/go-libstoracha/capabilities/blob"
+	"github.com/storacha/go-libstoracha/testutil"
+	"github.com/storacha/go-ucanto/core/delegation"
+	"github.com/storacha/go-ucanto/core/invocation"
+	"github.com/storacha/go-ucanto/core/message"
+	"github.com/storacha/go-ucanto/core/receipt"
+	"github.com/storacha/go-ucanto/core/receipt/ran"
+	"github.com/storacha/go-ucanto/core/result"
+	"github.com/storacha/go-ucanto/transport/car"
+	"github.com/storacha/go-ucanto/ucan"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRetrieve(t *testing.T) {
+	retrieveCaveats := blob.RetrieveCaveats{
+		Blob: blob.Blob{Digest: testutil.RandomMultihash(t)},
+	}
+
+	dlg, err := delegation.Delegate(
+		testutil.Bob,
+		testutil.Alice,
+		[]ucan.Capability[ucan.NoCaveats]{
+			ucan.NewCapability(blob.RetrieveAbility, testutil.Bob.DID().String(), ucan.NoCaveats{}),
+		},
+	)
+	require.NoError(t, err)
+
+	inv, err := blob.Retrieve.Invoke(
+		testutil.Alice,
+		testutil.Bob,
+		testutil.Bob.DID().String(),
+		retrieveCaveats,
+		delegation.WithProof(delegation.FromDelegation(dlg)),
+	)
+	require.NoError(t, err)
+
+	t.Run("round trip", func(t *testing.T) {
+		retrieveOk := blob.RetrieveOk{}
+
+		r0, err := receipt.Issue(
+			testutil.Bob,
+			result.Ok[blob.RetrieveOk, blob.RetrieveError](retrieveOk),
+			ran.FromInvocation(inv),
+		)
+		require.NoError(t, err)
+
+		// round trip the invocation and receipt in an agent message to ensure the
+		// invocation can be encoded and the receipt decoded
+		msg := roundTripAgentMessage(t, []invocation.Invocation{inv}, []receipt.AnyReceipt{r0})
+
+		rcptLink, ok := msg.Get(inv.Link())
+		require.True(t, ok)
+
+		reader, err := blob.NewRetrieveReceiptReader()
+		require.NoError(t, err)
+
+		r1, err := reader.Read(rcptLink, msg.Blocks())
+		require.NoError(t, err)
+
+		o, x := result.Unwrap(r1.Out())
+		require.Empty(t, x)
+		require.Equal(t, retrieveOk, o)
+	})
+
+	t.Run("round trip with error", func(t *testing.T) {
+		retrieveErr := blob.RetrieveError{
+			Name:    "NotFound",
+			Message: "I do not have it",
+		}
+
+		r0, err := receipt.Issue(
+			testutil.Bob,
+			result.Error[blob.RetrieveOk](retrieveErr),
+			ran.FromInvocation(inv),
+		)
+		require.NoError(t, err)
+
+		msg := roundTripAgentMessage(t, []invocation.Invocation{inv}, []receipt.AnyReceipt{r0})
+
+		rcptLink, ok := msg.Get(inv.Link())
+		require.True(t, ok)
+
+		reader, err := blob.NewRetrieveReceiptReader()
+		require.NoError(t, err)
+
+		r1, err := reader.Read(rcptLink, msg.Blocks())
+		require.NoError(t, err)
+
+		o, x := result.Unwrap(r1.Out())
+		require.Empty(t, o)
+		require.Equal(t, retrieveErr.Name, x.Name)
+		require.Equal(t, retrieveErr.Message, x.Message)
+	})
+}
+
+func roundTripAgentMessage(t *testing.T, invs []invocation.Invocation, rcpts []receipt.AnyReceipt) message.AgentMessage {
+	t.Helper()
+	inMsg, err := message.Build(invs, rcpts)
+	require.NoError(t, err)
+
+	outCodec := car.NewOutboundCodec()
+	req, err := outCodec.Encode(inMsg)
+	require.NoError(t, err)
+
+	inCodec, err := car.NewInboundCodec().Accept(req)
+	require.NoError(t, err)
+
+	outMsg, err := inCodec.Decoder().Decode(req)
+	require.NoError(t, err)
+
+	return outMsg
+}
