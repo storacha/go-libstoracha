d8083a0b9955078131a9c6b5473bd6e9030a660e
hannahhoward <hannah@hannahhoward.net>
2025-11-05 18:41:49 -0800

    feat(publisher): seperate generate ad and publish
    seperate a parallizable step (generating ads) from one that is not parallizable (publishing).
    
    - create a generate ad function that just generates the add that will be
      published for a given set of CIDs
    - create an advertisement publisher that can batch several ads together
      with only one head publish step
    - port the generic queuepoller into go-libstoracha, now supporting
      handlers that batch all jobs at once
    - modify the queue interface to specifically seperate the job from the
      ID on read
    - rename queues to be called `publishing` rather than `publisher`
      (consistency)
    - modify extended queue to match queue interface
    - add advertisement publishing SQS queue
    - generate new handlers and interfaces for double step queue publishing

---
+++ ipnipublisher/queue/aws/sqsadvertisementpublishingqueue.go
@@ -0,0 +1,141 @@
+package aws
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/google/uuid"
+	"github.com/ipni/go-libipni/ingest/schema"
+	"github.com/storacha/go-libstoracha/queuepoller"
+	ipldjson "github.com/storacha/go-ucanto/core/ipld/codec/json"
+)
+
+type advMarshaller schema.Advertisement
+
+func (a advMarshaller) MarshalJSON() ([]byte, error) {
+	return ipldjson.Encode(schema.Advertisement(a), schema.AdvertisementPrototype.Type())
+}
+func (a *advMarshaller) UnmarshalJSON(data []byte) error {
+	var adv schema.Advertisement
+	err := ipldjson.Decode(data, &adv, schema.AdvertisementPrototype.Type())
+	if err != nil {
+		return err
+	}
+	*a = advMarshaller(adv)
+	return nil
+}
+
+// queueMessage is the struct that is serialized onto an SQS message queue in JSON
+type queueMessage struct {
+	JobID         uuid.UUID     `json:"JobID,omitempty"`
+	Advertisement advMarshaller `json:"Message,omitempty"`
+}
+
+// SQSAdvertisementPublishingQueue implements a queue for publishing advertisements using SQS
+type SQSAdvertisementPublishingQueue struct {
+	queueID   string
+	sqsClient *sqs.Client
+	decoder   *SQSAdvertisementPublishingDecoder
+}
+
+// NewSQSAdvertisementPublishingQueue returns a new SQSAdvertisementPublishingQueue for the given aws config
+func NewSQSAdvertisementPublishingQueue(cfg aws.Config, queueID string) *SQSAdvertisementPublishingQueue {
+	return &SQSAdvertisementPublishingQueue{
+		queueID:   queueID,
+		sqsClient: sqs.NewFromConfig(cfg),
+		decoder:   NewSQSAdvertisementPublishingDecoder(),
+	}
+}
+
+// Queue sends a new advertisement publishing job to the SQS queue.
+func (s *SQSAdvertisementPublishingQueue) Queue(ctx context.Context, adv schema.Advertisement) error {
+	uuid := uuid.New()
+	messageJSON, err := json.Marshal(queueMessage{
+		JobID:         uuid,
+		Advertisement: advMarshaller(adv),
+	})
+	if err != nil {
+		return fmt.Errorf("serializing message json: %w", err)
+	}
+	message := &sqs.SendMessageInput{
+		QueueUrl:    aws.String(s.queueID),
+		MessageBody: aws.String(string(messageJSON)),
+	}
+	_, err = s.sqsClient.SendMessage(ctx, message)
+	if err != nil {
+		return fmt.Errorf("enqueueing message: %w", err)
+	}
+	return nil
+}
+
+// Read reads up to maxJobs advertisements from the SQS queue.
+func (s *SQSAdvertisementPublishingQueue) Read(ctx context.Context, maxJobs int) ([]queuepoller.WithID[schema.Advertisement], error) {
+	receiveOutput, err := s.sqsClient.ReceiveMessage(ctx, &sqs.ReceiveMessageInput{
+		QueueUrl:            aws.String(s.queueID),
+		MaxNumberOfMessages: int32(maxJobs),
+		WaitTimeSeconds:     20, // enable long-polling
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to receive messages from SQS: %w", err)
+	}
+
+	if len(receiveOutput.Messages) == 0 {
+		return []queuepoller.WithID[schema.Advertisement]{}, nil
+	}
+
+	ads := make([]queuepoller.WithID[schema.Advertisement], 0, len(receiveOutput.Messages))
+	for _, msg := range receiveOutput.Messages {
+		ad, err := s.decoder.DecodeMessage(ctx, aws.ToString(msg.ReceiptHandle), aws.ToString(msg.Body))
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode message: %w", err)
+		}
+
+		ads = append(ads, ad)
+	}
+
+	return ads, nil
+}
+
+func (s *SQSAdvertisementPublishingQueue) Release(ctx context.Context, jobID string) error {
+	_, err := s.sqsClient.ChangeMessageVisibility(ctx, &sqs.ChangeMessageVisibilityInput{
+		QueueUrl:          aws.String(s.queueID),
+		ReceiptHandle:     aws.String(jobID),
+		VisibilityTimeout: 0,
+	})
+
+	return err
+}
+
+// Delete deletes a job message from the SQS publisher queue.
+func (s *SQSAdvertisementPublishingQueue) Delete(ctx context.Context, jobID string) error {
+	_, err := s.sqsClient.DeleteMessage(ctx, &sqs.DeleteMessageInput{
+		QueueUrl:      aws.String(s.queueID),
+		ReceiptHandle: aws.String(jobID),
+	})
+	return err
+}
+
+// SQSAdvertisementPublishingDecoder provides interfaces for working with advertisements received over SQS
+type SQSAdvertisementPublishingDecoder struct {
+}
+
+// NewSQSDecoder returns a new decoder for the given AWS config
+func NewSQSAdvertisementPublishingDecoder() *SQSAdvertisementPublishingDecoder {
+	return &SQSAdvertisementPublishingDecoder{}
+}
+
+// DecodeMessage decodes a provider caching job from the SQS message body, reading the stored index from S3
+func (s *SQSAdvertisementPublishingDecoder) DecodeMessage(ctx context.Context, receiptHandle string, messageBody string) (queuepoller.WithID[schema.Advertisement], error) {
+	var decodedMsg queueMessage
+	err := json.Unmarshal([]byte(messageBody), &decodedMsg)
+	if err != nil {
+		return queuepoller.WithID[schema.Advertisement]{}, fmt.Errorf("deserializing message: %w", err)
+	}
+	return queuepoller.WithID[schema.Advertisement]{
+		ID:  receiptHandle,
+		Job: schema.Advertisement(decodedMsg.Advertisement),
+	}, nil
+}
---
+++ queuepoller/queuepoller.go
@@ -0,0 +1,325 @@
+package queuepoller
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+
+	logging "github.com/ipfs/go-log/v2"
+	"github.com/storacha/go-libstoracha/jobqueue"
+)
+
+const (
+	defaultJobBatchSize = 10
+	defaultConcurrency  = 100
+
+	maxJobBatchSize      = 10
+	maxJobProcessingTime = 5 * time.Minute
+)
+
+var log = logging.Logger("queuepoller")
+
+type (
+	WithID[Job any] struct {
+		ID  string
+		Job Job
+	}
+
+	// QueueQueuer is an interface for queuing jobs.
+	QueueQueuer[Job any] interface {
+		Queue(ctx context.Context, job Job) error
+	}
+
+	// QueueReader is an interface for reading jobs from the queue.
+	QueueReader[Job any] interface {
+		Read(ctx context.Context, maxJobs int) ([]WithID[Job], error)
+	}
+
+	// QueueReleaser is an interface for releasing jobs from the queue.
+	QueueReleaser interface {
+		Release(ctx context.Context, jobID string) error
+	}
+
+	// QueueDeleter is an interface for deleting jobs from the queue.
+	QueueDeleter interface {
+		Delete(ctx context.Context, jobID string) error
+	}
+
+	// Queue is an interface for a job queue,
+	// combining queuing, reading, releasing, and deleting jobs.
+	Queue[Job any] interface {
+		QueueQueuer[Job]
+		QueueReader[Job]
+		QueueReleaser
+		QueueDeleter
+	}
+
+	// config
+	config struct {
+		jobBatchSize int
+		concurrency  int
+	}
+
+	// Option configures the CachingQueuePoller
+	Option func(*config)
+
+	// Handler processes jobs of the given type.
+	Handler[Job any] interface {
+		toJobQueue(queue Queue[Job], concurrency int) jobQueue[Job]
+	}
+
+	singleHandler[Job any] struct {
+		handler func(ctx context.Context, j Job) error
+	}
+
+	batchHandler[Job any] struct {
+		handler func(ctx context.Context, jobs []WithID[Job]) map[string]error
+	}
+
+	jobQueue[Job any] interface {
+		Startup()
+		Shutdown(ctx context.Context) error
+		Queue(ctx context.Context, job []WithID[Job]) error
+	}
+
+	singleJobQueue[Job any] struct {
+		jq *jobqueue.JobQueue[WithID[Job]]
+	}
+
+	batchJobQueue[Job any] struct {
+		jq *jobqueue.JobQueue[[]WithID[Job]]
+	}
+
+	// QueuePoller polls a queue for  jobs and processes them
+	// using the provided JobHandler.
+	QueuePoller[Job any] struct {
+		queue        Queue[Job]
+		handler      Handler[Job]
+		jq           jobQueue[Job]
+		jobBatchSize int
+		ctx          context.Context
+		cancel       context.CancelFunc
+		stopped      chan struct{}
+		startOnce    sync.Once
+		stopOnce     sync.Once
+	}
+)
+
+// WithJobBatchSize sets the maximum number of jobs to process in a batch
+func WithJobBatchSize(size int) Option {
+	return func(cfg *config) {
+		cfg.jobBatchSize = size
+	}
+}
+
+// WithConcurrency sets the maximum number of concurrent job processing
+func WithConcurrency(concurrency int) Option {
+	return func(cfg *config) {
+		cfg.concurrency = concurrency
+	}
+}
+
+// JobHandler creates a Handler from a function that processes single jobs.
+func JobHandler[Job any](handler func(ctx context.Context, j Job) error) Handler[Job] {
+	return &singleHandler[Job]{handler: handler}
+}
+
+// BatchJobHandler creates a Handler from a function that processes multiple jobs.
+func BatchJobHandler[Job any](handler func(ctx context.Context, jobs []WithID[Job]) map[string]error) Handler[Job] {
+	return &batchHandler[Job]{handler: handler}
+}
+
+// NewQueuePoller creates a new QueuePoller instance.
+func NewQueuePoller[Job any](queue Queue[Job], handler Handler[Job], opts ...Option) (*QueuePoller[Job], error) {
+	cfg := &config{
+		jobBatchSize: defaultJobBatchSize,
+		concurrency:  defaultConcurrency,
+	}
+
+	for _, opt := range opts {
+		opt(cfg)
+	}
+
+	poller := &QueuePoller[Job]{
+		queue:        queue,
+		handler:      handler,
+		jq:           handler.toJobQueue(queue, cfg.concurrency),
+		jobBatchSize: cfg.jobBatchSize,
+		stopped:      make(chan struct{}),
+	}
+
+	if poller.jobBatchSize > maxJobBatchSize {
+		return nil, fmt.Errorf("job batch size %d exceeds maximum allowed %d", poller.jobBatchSize, maxJobBatchSize)
+	}
+
+	return poller, nil
+}
+
+// Start begins polling the queue for caching jobs.
+func (p *QueuePoller[Job]) Start() {
+	p.startOnce.Do(func() {
+		// Create root context
+		p.ctx, p.cancel = context.WithCancel(context.Background())
+		p.jq.Startup()
+		log.Info("Starting caching queue poller")
+
+		go func() {
+			for {
+				select {
+				case <-p.ctx.Done():
+					log.Info("Stopping polling loop")
+					close(p.stopped)
+					return
+				default:
+					p.processJobs(p.ctx)
+				}
+			}
+		}()
+	})
+}
+
+// Stop stops the polling loop and waits for it to finish.
+func (p *QueuePoller[Job]) Stop() {
+	p.stopOnce.Do(func() {
+		// Cancel the root context, which will cancel all child contexts
+		if p.cancel != nil {
+			p.cancel()
+		}
+
+		// Wait for the polling loop to finish
+		<-p.stopped
+
+		p.jq.Shutdown(p.ctx)
+	})
+}
+
+// processJobs reads and processes all available jobs from the queue in batches
+func (p *QueuePoller[Job]) processJobs(ctx context.Context) {
+	// Read a batch of jobs and queue them in the job queue
+	jobs, err := p.queue.Read(ctx, p.jobBatchSize)
+	if err != nil {
+		log.Errorf("Error reading jobs from queue: %v", err)
+		return
+	}
+
+	err = p.jq.Queue(ctx, jobs)
+	if err != nil {
+		log.Errorf("Error queuing jobs: %v", err)
+	}
+}
+
+func (s *singleHandler[Job]) toJobQueue(queue Queue[Job], concurrency int) jobQueue[Job] {
+	handler := jobqueue.JobHandler(func(ctx context.Context, job WithID[Job]) error {
+		jobCtx, cancel := context.WithTimeout(ctx, maxJobProcessingTime)
+		defer cancel()
+
+		// Process the job
+		err := s.handler(jobCtx, job.Job)
+		if err != nil && !errors.Is(err, context.DeadlineExceeded) {
+			// if the error is not a timeout, make the job visible so that it can be retried
+			if err := queue.Release(ctx, job.ID); err != nil {
+				log.Warnf("Failed to release job %s: %s", job.ID, err)
+			}
+
+			return fmt.Errorf("failed to perform job %s: %w", job.ID, err)
+		}
+
+		// Do not hold up the queue by re-attempting a job that times out.
+		// Log the error and proceed with deletion.
+		if errors.Is(err, context.DeadlineExceeded) {
+			log.Warnf("Not retrying provider job for %s: %s", job.ID, err)
+		}
+
+		// Delete the job too if processing was successful
+		if err := queue.Delete(ctx, job.ID); err != nil {
+			return fmt.Errorf("failed to delete job %s: %w", job.ID, err)
+		}
+
+		return nil
+	})
+	return &singleJobQueue[Job]{
+		jq: jobqueue.NewJobQueue[WithID[Job]](
+			handler,
+			jobqueue.WithConcurrency(concurrency),
+			jobqueue.WithErrorHandler(func(err error) {
+				log.Errorw("processing job", "error", err)
+			})),
+	}
+}
+
+func (s *singleJobQueue[Job]) Startup() {
+	s.jq.Startup()
+}
+
+func (s *singleJobQueue[Job]) Shutdown(ctx context.Context) error {
+	return s.jq.Shutdown(ctx)
+}
+
+func (s *singleJobQueue[Job]) Queue(ctx context.Context, job []WithID[Job]) error {
+	for _, j := range job {
+		if err := s.jq.Queue(ctx, j); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (b *batchHandler[Job]) toJobQueue(queue Queue[Job], concurrency int) jobQueue[Job] {
+	handler := jobqueue.JobHandler(func(ctx context.Context, jobs []WithID[Job]) error {
+		jobCtx, cancel := context.WithTimeout(ctx, maxJobProcessingTime)
+		defer cancel()
+
+		// Process the jobs
+		errMap := b.handler(jobCtx, jobs)
+
+		// Handle individual job results
+		for _, job := range jobs {
+			jobErr := errMap[job.ID]
+			if jobErr != nil && !errors.Is(jobErr, context.DeadlineExceeded) {
+				// if the error is not a timeout, make the job visible so that it can be retried
+				if err := queue.Release(ctx, job.ID); err != nil {
+					log.Warnf("Failed to release job %s: %s", job.ID, err)
+				}
+
+				log.Errorf("Failed to perform job %s: %s", job.ID, jobErr)
+				continue
+			}
+
+			// Do not hold up the queue by re-attempting a job that times out.
+			// Log the error and proceed with deletion.
+			if errors.Is(jobErr, context.DeadlineExceeded) {
+				log.Warnf("Not retrying provider job for %s: %s", job.ID, jobErr)
+			}
+
+			// Delete the job too if processing was successful
+			if err := queue.Delete(ctx, job.ID); err != nil {
+				log.Errorf("Failed to delete job %s: %s", job.ID, err)
+			}
+		}
+
+		return nil
+	})
+	return &batchJobQueue[Job]{
+		jq: jobqueue.NewJobQueue[[]WithID[Job]](
+			handler,
+			jobqueue.WithConcurrency(concurrency),
+			jobqueue.WithErrorHandler(func(err error) {
+				log.Errorw("processing job", "error", err)
+			}),
+		),
+	}
+}
+
+func (b *batchJobQueue[Job]) Startup() {
+	b.jq.Startup()
+}
+
+func (b *batchJobQueue[Job]) Shutdown(ctx context.Context) error {
+	return b.jq.Shutdown(ctx)
+}
+
+func (b *batchJobQueue[Job]) Queue(ctx context.Context, jobs []WithID[Job]) error {
+	return b.jq.Queue(ctx, jobs)
+}
